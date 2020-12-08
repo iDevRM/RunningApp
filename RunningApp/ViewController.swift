2a3e094da5d8b26e4904716a6668a94411a6eacf
Rick <rmartinez1611@gmail.com>
2020-12-08 15:50:26 -0500

    Initial Commit

---
+++ RunningApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RunningApp
+//
+//  Created by Ricardo Martinez on 12/8/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
